17a7e9a7971bf0c744eac2aee46ffb632cc73795
Deinonzch <slagol@st.amu.edu.pl>
2016-12-18 20:06:08 +0100

    Translate

--- Translation/FETP.py
+++ Translation/FETP.py
@@ -3,6 +3,6 @@ import sys
 import re
 
 for line in sys.stdin:
-	words = re.findall(r'(\"[A-Za-zĘÓŁŚĄŻŹĆŃęółśążźćń0-9\.\\:,\s\'\?\!\-—\(\)"]+(\.|\?|\!|,|\\|—|-|\")\")',line)
+	words = re.findall(r'(\"[A-Za-zĘÓŁŚĄŻŹĆŃęółśążźćń0-9\.\\:,\s\'\?\!\-—\(\)~☆*"]+(\.|\?|\!|,|\\|—|-|\"|~☆*)\")',line)
 	if words:
 		print(words[0][0] + "\n")
