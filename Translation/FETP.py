759e485ed404a694e9685cabe6f6ea2d0547cf77
Deinonzch <Deinonzch@users.noreply.github.com>
2017-09-07 12:06:16 +0200

    git add onik_009

--- Translation/FETP.py
+++ Translation/FETP.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import sys
 import re
 
 for line in sys.stdin:
-	words = re.findall(r'(\"[A-Za-zĘÓŁŚĄŻŹĆŃęółśążźćń0-9\.\\:,\s\'\?\!\-—\(\)~☆*=+;"]+(\.|\?|\'|\!|,|\\|—|-|\"|~☆*)\")',line)
+	words = re.findall(r'(\"[A-Za-zĘÓŁŚĄŻŹĆŃęółśążźćń0-9\.\\:,\s\'\?\!\-—\(\)~☆♪*=+;"]+(\.|\?|\'|\!|,|\\|—|-|\"|~|☆|♪|\*)\")',line)
 	if words:
 		print(words[0][0] + "\n")
