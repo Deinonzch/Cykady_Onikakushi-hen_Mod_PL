68531072656cce0fc97a5bf00c2e7d7d44a106c3
Deinonzch <slagol@st.amu.edu.p>
2018-01-23 08:48:13 +0100

    Add change files and pl files onik_012 & tips 16 and 17

--- Translation/ChangeFile.py
+++ Translation/ChangeFile.py
@@ -6,10 +6,10 @@ import re
 
 reg = re.compile(r'NULL, (\"(?!\\n)[A-Za-zĘÓŁŚĄŻŹĆŃęółśążźćń0-9\.\\:,\s\'\?\!\-—\(\)~☆♪「」*=+;><"]+(\.|\?|\'|\!|,|\\|—|-|\"|~|\)|☆|♪|\*|[A-Za-zĘÓŁŚĄŻŹĆŃęółśążźćń0-9])\")')
 
-#f = open('onik_011.txt', 'r')
-#g = open('onik_011_pl.txt', 'r')
-f = open('onik_tips_15.txt', 'r')
-g = open('onik_tips_15_pl.txt', 'r')
+f = open('onik_012.txt', 'r')
+g = open('onik_012_pl.txt', 'r')
+#f = open('onik_tips_17.txt', 'r')
+#g = open('onik_tips_17_pl.txt', 'r')
 lines = g.readlines()
 i=0
 for line in f:
